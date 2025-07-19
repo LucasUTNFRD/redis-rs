12c9e89547bfc44eb2904145d24144a25d185b91
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2025-07-19 14:04:22 -0300

    implement pong response

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
+
+const PONG_RESPONSE: &[u8] = b"=PONG\r\n";
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write_all(PONG_RESPONSE).unwrap();
+                // println!("accepted new connection");
             }
             Err(e) => {
                 println!("error: {}", e);
