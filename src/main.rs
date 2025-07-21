946d8b8b4fd50630225f494ac80755233a19d569
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2025-07-21 16:31:35 -0300

    chore: use While let instead of loop + tokio::select macro

--- src/main.rs
+++ src/main.rs
@@ -65,81 +65,84 @@ impl KvStore {
 async fn handle_connection(conn: &mut TcpStream, redis_state: &KvStore) -> Result<()> {
     let mut framed = Framed::new(conn, RespCodec);
 
-    loop {
-        tokio::select! {
-            resp_result = framed.next() => {
-                match resp_result {
-                    Some(resp) => {
-                        let resp_data= resp.context("Decoding failed")?;
-                        match resp_data {
-                            RespDataType::Array(ref parts) if !parts.is_empty() => {
-                                match &parts[0] {
-                                    RespDataType::BulkString(cmd) | RespDataType::SimpleString(cmd) => {
-                                        match cmd.to_uppercase().as_str() {
-                                            "PING" => {
-                                                let response = RespDataType::SimpleString("PONG".to_string());
-                                                framed.send(response).await?;
-                                            }
-                                            "ECHO" => {
-                                                if parts.len() > 1 {
-                                                    if let RespDataType::BulkString(msg) = &parts[1] {
-                                                        let response = RespDataType::BulkString(msg.clone());
-                                                        framed.send(response).await?;
-                                                    }
-                                                }
-                                            }
-                                            "GET" => {
-                                                if parts.len() == 2 {
-                                                      if let RespDataType::BulkString(key) | RespDataType::SimpleString(key) = &parts[1] {
-                                                            let value = redis_state.get(key.clone()).await;
-                                                            framed.send(value).await?;
-                                                        } else {
-                                                            let response = RespDataType::SimpleError("ERR invalid key type".to_string());
-                                                            framed.send(response).await?;
-                                                        }
-                                            } else {
-                                                        let response = RespDataType::SimpleError("ERR wrong number of arguments for 'get' command".to_string());
-                                                        framed.send(response).await?;
-                                                }
-                                            }
-                                            "SET" => {
-                                                 if parts.len() == 3 {
-                                                         if let (RespDataType::BulkString(key) | RespDataType::SimpleString(key),
-                                                                 RespDataType::BulkString(value) | RespDataType::SimpleString(value)) = (&parts[1], &parts[2]) {
-                                                             let response = redis_state.set(key.clone(), value.clone()).await;
-                                                             framed.send(response).await?;
-                                                        } else {
-                                                            let response = RespDataType::SimpleError("ERR invalid argument types".to_string());
-                                                            framed.send(response).await?;
-                                                        }
-                                                 } else {
-                                                     let response = RespDataType::SimpleError("ERR wrong number of arguments for 'set' command".to_string());
-                                                     framed.send(response).await?;
-                                                 }
-                                            }
-                                            _ => {
-                                                let response = RespDataType::SimpleError("ERR unknown command".to_string());
-                                                framed.send(response).await?;
-                                            }
-                                        }
-                                    }
-                                    _ => {
-                                        let response = RespDataType::SimpleError("ERR invalid command format".to_string());
-                                        framed.send(response).await?;
-                                    }
+    while let Some(resp_result) = framed.next().await {
+        let resp_data = resp_result.context("Decoding failed")?;
+        match resp_data {
+            RespDataType::Array(ref parts) if !parts.is_empty() => match &parts[0] {
+                RespDataType::BulkString(cmd) | RespDataType::SimpleString(cmd) => {
+                    match cmd.to_uppercase().as_str() {
+                        "PING" => {
+                            let response = RespDataType::SimpleString("PONG".to_string());
+                            framed.send(response).await?;
+                        }
+                        "ECHO" => {
+                            if parts.len() > 1 {
+                                if let RespDataType::BulkString(msg) = &parts[1] {
+                                    let response = RespDataType::BulkString(msg.clone());
+                                    framed.send(response).await?;
                                 }
                             }
-                            _ => {
-                                let response = RespDataType::SimpleError("ERR commands must be arrays".to_string());
+                        }
+                        "GET" => {
+                            if parts.len() == 2 {
+                                if let RespDataType::BulkString(key)
+                                | RespDataType::SimpleString(key) = &parts[1]
+                                {
+                                    let value = redis_state.get(key.clone()).await;
+                                    framed.send(value).await?;
+                                } else {
+                                    let response = RespDataType::SimpleError(
+                                        "ERR invalid key type".to_string(),
+                                    );
+                                    framed.send(response).await?;
+                                }
+                            } else {
+                                let response = RespDataType::SimpleError(
+                                    "ERR wrong number of arguments for 'get' command".to_string(),
+                                );
                                 framed.send(response).await?;
                             }
                         }
-                    }
-                    None => {
-                        println!("Client disconnected.");
-                        break;
+                        "SET" => {
+                            if parts.len() == 3 {
+                                if let (
+                                    RespDataType::BulkString(key) | RespDataType::SimpleString(key),
+                                    RespDataType::BulkString(value)
+                                    | RespDataType::SimpleString(value),
+                                ) = (&parts[1], &parts[2])
+                                {
+                                    let response =
+                                        redis_state.set(key.clone(), value.clone()).await;
+                                    framed.send(response).await?;
+                                } else {
+                                    let response = RespDataType::SimpleError(
+                                        "ERR invalid argument types".to_string(),
+                                    );
+                                    framed.send(response).await?;
+                                }
+                            } else {
+                                let response = RespDataType::SimpleError(
+                                    "ERR wrong number of arguments for 'set' command".to_string(),
+                                );
+                                framed.send(response).await?;
+                            }
+                        }
+                        _ => {
+                            let response =
+                                RespDataType::SimpleError("ERR unknown command".to_string());
+                            framed.send(response).await?;
+                        }
                     }
                 }
+                _ => {
+                    let response =
+                        RespDataType::SimpleError("ERR invalid command format".to_string());
+                    framed.send(response).await?;
+                }
+            },
+            _ => {
+                let response = RespDataType::SimpleError("ERR commands must be arrays".to_string());
+                framed.send(response).await?;
             }
         }
     }
