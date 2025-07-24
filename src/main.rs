b2742c3fd4e2ba0ce50297fc16396cd540231253
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2025-07-24 19:47:55 -0300

    feat: add optional --port flag to configure server bind address

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 #![allow(unused_imports)]
 
 use anyhow::Result;
+use clap::{Arg, Command};
 use codecrafters_redis::server::RedisServer;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Logs from your program will appear here!");
+    let matches = Command::new("codecrafters-redis")
+        .arg(
+            Arg::new("port")
+                .long("port")
+                .value_name("PORT")
+                .help("Port to bind the Redis server to")
+                .default_value("6379"),
+        )
+        .get_matches();
 
-    let server = RedisServer::new("127.0.0.1:6379").await?;
+    let port = matches
+        .get_one::<String>("port")
+        .expect("default always present");
+
+    let addr = format!("127.0.0.1:{}", port);
+
+    let server = RedisServer::new(&addr).await?;
     server.run().await
 }
